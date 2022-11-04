a6edddff05bde707275c3cff4fcc22912111c26c
Ryan Wagoner <rwagoner@excalibur-partners.com>
2022-11-04 00:09:18 -0400

    Fix timezone by implementing S6 overlay

--- omnilink-bridge/data/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bashio
-CONFIG_PATH=/data/options.json
-
-jq -r 'to_entries|map("\(.key)=\(.value|tostring)")|.[]' $CONFIG_PATH > /app/OmniLinkBridge.ini
-
-mono /app/OmniLinkBridge.exe -i -c /app/OmniLinkBridge.ini
